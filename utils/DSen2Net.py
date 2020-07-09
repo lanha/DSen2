e07a9f54ae5decbe86898e256a5e31364041a141
Rodrigo Almeida <rodrigo.almeida@up42.com>
2020-07-09 18:17:02 +0200

    Apply black

--- utils/DSen2Net.py
+++ utils/DSen2Net.py
@@ -3,13 +3,17 @@ from keras.models import Model, Input
 from keras.layers import Conv2D, Concatenate, Activation, Lambda, Add
 import keras.backend as K
 
-K.set_image_data_format('channels_first')
+K.set_image_data_format("channels_first")
 
 
 def resBlock(x, channels, kernel_size=[3, 3], scale=0.1):
-    tmp = Conv2D(channels, kernel_size, kernel_initializer='he_uniform', padding='same')(x)
-    tmp = Activation('relu')(tmp)
-    tmp = Conv2D(channels, kernel_size, kernel_initializer='he_uniform', padding='same')(tmp)
+    tmp = Conv2D(
+        channels, kernel_size, kernel_initializer="he_uniform", padding="same"
+    )(x)
+    tmp = Activation("relu")(tmp)
+    tmp = Conv2D(
+        channels, kernel_size, kernel_initializer="he_uniform", padding="same"
+    )(tmp)
     tmp = Lambda(lambda x: x * scale)(tmp)
 
     return Add()([x, tmp])
@@ -26,13 +30,21 @@ def s2model(input_shape, num_layers=32, feature_size=256):
         x = Concatenate(axis=1)([input10, input20])
 
     # Treat the concatenation
-    x = Conv2D(feature_size, (3, 3), kernel_initializer='he_uniform', activation='relu', padding='same')(x)
+    x = Conv2D(
+        feature_size,
+        (3, 3),
+        kernel_initializer="he_uniform",
+        activation="relu",
+        padding="same",
+    )(x)
 
     for i in range(num_layers):
         x = resBlock(x, feature_size)
 
     # One more convolution, and then we add the output of our first conv layer
-    x = Conv2D(input_shape[-1][0], (3, 3), kernel_initializer='he_uniform', padding='same')(x)
+    x = Conv2D(
+        input_shape[-1][0], (3, 3), kernel_initializer="he_uniform", padding="same"
+    )(x)
     # x = Dropout(0.3)(x)
     if len(input_shape) == 3:
         x = Add()([x, input60])
@@ -41,4 +53,3 @@ def s2model(input_shape, num_layers=32, feature_size=256):
         x = Add()([x, input20])
         model = Model(inputs=[input10, input20], outputs=x)
     return model
-
