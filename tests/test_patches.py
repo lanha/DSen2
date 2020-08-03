9f4cf984d9f2b12bcebce30d9b4d94451301160a
Rodrigo Almeida <rodrigo.almeida@up42.com>
2020-08-03 14:19:22 +0200

    Add multiple tests

--- tests/test_patches.py
+++ tests/test_patches.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+import tempfile
 import pytest
 
 import numpy as np
@@ -20,6 +22,16 @@ def dset_60():
     return np.ones((1830, 1830, 3))
 
 
+@pytest.fixture()
+def scale_20():
+    return 2
+
+
+@pytest.fixture()
+def scale_60():
+    return 6
+
+
 def test_get_test_patches(dset_10, dset_20):
     r = patches.get_test_patches(dset_10, dset_20, 128, 8)
     assert len(r) == 2
@@ -35,9 +47,54 @@ def test_get_test_patches60(dset_10, dset_20, dset_60):
     assert r[2].shape == (4356, 3, 192, 192)
 
 
-def test_save_random_patches():
-    pass
+def save_test_patches(dset_10, dset_20):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        patches.save_test_patches(dset_10, dset_20, tmpdir, 128, 8)
+        f = Path(tmpdir).glob("*/**")
+        assert len(list(f)) == 2
 
 
-def test_save_random_patches60():
-    pass
+def save_test_patches60(dset_10, dset_20, dset_60):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        patches.save_test_patches60(dset_10, dset_20, dset_60, tmpdir, 192, 12)
+        f = Path(tmpdir).glob("*/**")
+        assert len(list(f)) == 3
+
+
+def test_save_random_patches(dset_10, dset_20, scale_20):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        patches.save_random_patches(
+            dset_20,
+            patches.downPixelAggr(dset_10, scale_20),
+            patches.downPixelAggr(dset_20, scale_20),
+            tmpdir,
+        )
+        f = Path(tmpdir).glob("*/**")
+        assert len(list(f)) == 2
+
+
+def test_save_random_patches60(dset_10, dset_20, dset_60, scale_60):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        patches.save_random_patches60(
+            dset_60,
+            patches.downPixelAggr(dset_10, scale_60),
+            patches.downPixelAggr(dset_20, scale_60),
+            patches.downPixelAggr(dset_60, scale_60),
+            tmpdir,
+        )
+        f = Path(tmpdir).glob("*/**")
+        assert len(list(f)) == 3
+
+
+def test_downPixelAggr(dset_10, scale_20):
+    r = patches.downPixelAggr(dset_10, scale_20)
+    assert r.shape == (1, 1, 1)
+
+
+def test_recompose_images(dset_10, dset_20):
+    p = patches.get_test_patches(dset_10, dset_20, 128, 8)
+    r_p = patches.recompose_images(p[0], 8)
+    assert dset_10.shape == r_p.shape
+
+def test_interp_patches(dset20, dset_10):
+    patches.interp_patches(dset20, dset10.shape)
